dc5e535b1ba5454049f06a39de0d1bf3d6408318
Muhammad <air.oamyst517@yahoo.com>
2018-10-12 19:15:57 +0200

    # Update analysis handler

--- modules/analysis_manager.py
+++ modules/analysis_manager.py
@@ -7,16 +7,18 @@
 
 
 import os
-import time
 import logging
 
-from lib.api.utils import get_package_name, get_filename_from_path
-from lib.definitions.constants import ANALYSES_DIR
-from modules.monitoring.api_mon import APIMonitor
-from modules.monitoring.tcpdump import TCPDUMP
-from modules.connection.droidbot import DroidBot
-from modules.connection.adb import ADB
 from modules import startup
+from modules.connection.adb import ADB
+from modules.connection.Frida import Frida
+from modules.connection.droidbot import DroidBot
+from modules.monitoring.api_mon import APIMonitor
+from modules.monitoring.net_mon import TrafficMonitor
+
+from lib.definitions.constants import ANALYSES_DIR
+from lib.definitions.exceptions import XenDroidModuleError
+from lib.api.utils import get_package_name, get_filename_from_path
 
 
 class AnalysisManager(object):
@@ -26,25 +28,68 @@ class AnalysisManager(object):
     """
 
     def __init__(self, apk_path, device_serial):
-
         self.logger = logging.getLogger(self.__class__.__name__)
 
         self.apk_path = apk_path
-        self.device_serial = device_serial
         self.adb_connection = ADB(device_serial)
+        self.frida_connection = Frida(device_serial)
 
-        p = ANALYSES_DIR
-        analysis_num = 0 if not os.listdir(p) else int(max(os.listdir(p), key=int))+1
-        self.analysis_path = os.path.join(p, analysis_num)
+        self.modules = []
 
+        self.analysis_path = None
+        self.backup_path = None
+        self._init_paths()
+
+    def _init_paths(self):
+        analysis_num = 0
+        if os.path.exists(ANALYSES_DIR):
+            analysis_num = int(max(os.listdir(ANALYSES_DIR),
+                                   key=lambda x: int(x.split('_')[1]))) + 1
+
+        self.analysis_path = os.path.join(ANALYSES_DIR, 'task_' + str(analysis_num))
         self.backup_path = os.path.join(self.analysis_path, 'backup', 'backup.ab')
-        os.makedirs(self.backup_path[0:len('backup.ab')])
+
+        os.makedirs(os.path.join(self.analysis_path, 'backup'))
+        os.makedirs(os.path.join(self.analysis_path, 'logs'))
+
+    def stop_monitoring(self):
+        for module in self.modules:
+            try:
+                module.stop()
+            except XenDroidModuleError:
+                self.logger.error(
+                    'Failed at finalizing the capturing process for the '
+                    '{} module, some logs might be missing!'.format(module.description)
+                )
+        self.modules = []
+
+    def load_monitoring(self):
+        modules = self.modules
+
+        # Initialize the monitoring modules
+        tm = TrafficMonitor(self.adb_connection, self.analysis_path)
+        api_m = APIMonitor(self.frida_connection, self.analysis_path)
+
+        try:
+            api_m.start()
+            modules.append(api_m)
+        except XenDroidModuleError:
+            self.logger.error('API monitoring module startup failed...')
+
+        try:
+            tm.start()
+            modules.append(tm)
+        except XenDroidModuleError:
+            self.logger.error('Network sniffer module startup failed...')
+
+    def roll_back(self):
+        if self.backup_path.exists():
+            self.adb_connection.restore_backup(self.backup_path)
 
     def start(self):
-
         self.logger.info('Analysis started with task ID: '
                          + get_filename_from_path(self.analysis_path))
-        self.adb_connection.unlock()
+
         # store a backup of the device's current state
         self.adb_connection.backup_device(self.backup_path)
         startup.run_startup(self.adb_connection)
@@ -54,26 +99,14 @@ class AnalysisManager(object):
 
         # fire the target application
         package_name = get_package_name(self.apk_path)
-        pid = self.adb_connection.get_pid_from_package_name(package_name)
-        self.adb_connection.start_app(package_name)
+        self.frida_connection.spawn_app(package_name)
 
-        # start api hooking ... but suspend target app before
-        self.adb_connection.suspend_process(pid)
+        # start loading the monitoring modules
+        self.load_monitoring()
 
-        api_monitor = APIMonitor(package_name, self.device_serial, self.analysis_path)
-        api_monitor.start()
-
-        tcpdump = TCPDUMP(self.adb_connection, self.analysis_path)
-        tcpdump.start()
-
-        self.adb_connection.resume_process(pid)
-
-        # interact with the device using DroidBot: https://github.com/honeynet/droidbot
+        # interact with the device using DroidBot
         DroidBot().interact()
 
-        time.sleep(1)
-
-        ############################
         ############################
 
-        self.adb_connection.restore_backup(self.backup_path)
+        self.stop_monitoring()
